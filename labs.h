d2c06ad6e86032480882f0fe7e212f28f4c29957
Kristina <kristinazaravina86@gmail.com>
2025-05-15 11:36:17 +0300

    Update labs.h

--- labs.h
+++ labs.h
@@ -1,12 +1,9 @@
-#define  labs.h
 #include <stdio.h> 
 void lab1();
 void lab1add();
 void lab2();
 void lab3();
 void lab4();
-void lab4add();
 void lab5();
 void lab5add();
 void lab6();
-void lab6add();
